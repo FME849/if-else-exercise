7bafe1af76e12561e41bbe26b210782db4aa405f
FME849 <1414439@gmail.com>
2021-10-10 11:41:04 +0700

    bài 3

--- main.js
+++ main.js
@@ -44,12 +44,11 @@ document.getElementById("num-order").onclick = orderNum;
  * Khối 2: 
  *  Gán giá trị cho biến char
  *  Lập công thức switch case:
- *      char = L => "Xin chào Người lạ ơi!"
- *      char = B => "Xin chào Bố!"
- *      char = M => "Xin chào Mẹ!"
- *      char = A => "Xin chào Anh Trai!"
- *      char = E => "Xin chào Em Gái!"
- *  Hiện lời chào ở input#greeting
+ *      char = L => Hiện lời chào "Xin chào Người lạ ơi!"
+ *      char = B => Hiện lời chào "Xin chào Bố!"
+ *      char = M => Hiện lời chào "Xin chào Mẹ!"
+ *      char = A => Hiện lời chào "Xin chào Anh Trai!"
+ *      char = E => Hiện lời chào "Xin chào Em Gái!"
  * Khối 3:
  *  Lời chào thành viên được chọn
  */
@@ -75,3 +74,28 @@ function greetPeople() {
     }
 }
 document.getElementById("greetingPeople").onclick = greetPeople;
+
+// Ex 03
+/**
+ * Khối 1:
+ *  3 số nguyên dương num1, num2, num3
+ * Khối 2:
+ *  Gán giá trị cho biến num1, num2, num3, numEven, numOdd
+ *  Lập công thức so sánh phần dư khi chia 2:
+ *      if num1%2 == 0 => numEven+=1 else numOdd+=1;
+ * Khối 3:
+ *  Số lượng số chẵn numEven, số lẻ numOdd
+ */
+
+function countOddEven() {
+    var num1 = Number(document.getElementById("ex03-num1").value)
+    var num2 = Number(document.getElementById("ex03-num2").value)
+    var num3 = Number(document.getElementById("ex03-num3").value)
+    var numEven = 0;
+    var numOdd = 0;
+    num1%2 == 0 ? numEven+=1 : numOdd+=1;
+    num2%2 == 0 ? numEven+=1 : numOdd+=1;
+    num3%2 == 0 ? numEven+=1 : numOdd+=1;
+    document.getElementById("numOddEven").value = "Có " + numEven + " số chẵn và " + numOdd + " số lẻ";
+}
+document.getElementById("countOddEven").onclick = countOddEven;
